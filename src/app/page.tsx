24cd55c3a367aca1338bff047db92e75d9f64ea6
waseem <waseemazmy21@gmail.com>
2024-04-12 03:16:09 +0200

    Add home page hero seciton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/home/hero";
+
 export default function Home() {
-  return <main> </main>;
+  return (
+    <section>
+      <Hero />
+    </section>
+  );
 }
---
+++ src/components/home/hero.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+function Hero() {
+  return (
+    <section className=" py-16">
+      <div className="container flex flex-col-reverse gap-8 sm:flex-row">
+        <div className="flex items-center justify-center sm:flex-1">
+          <h1 className=" text-gradient max-w-[13ch] flex-1 p-1 text-center text-4xl font-bold leading-[1.2] lg:text-6xl">
+            المستقبل يبدأ
+            <br /> هنا
+          </h1>
+        </div>
+
+        <div className="flex items-center justify-center sm:flex-1">
+          <Image
+            src="/images/home/hero.png"
+            width={729}
+            height={456}
+            alt=""
+            className="w-full max-w-[350px] object-cover sm:max-w-3xl"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
