203132bd588d1c1fd8f7fa95ec39b2a5b16166c2
waseem <waseemazmy21@gmail.com>
2024-04-18 19:58:36 +0200

    Add craftsmen and clients routes

---
+++ src/app/clients/[id]/page.tsx
@@ -0,0 +1,9 @@
+function ClientPage({ params }: { params: { id: string } }) {
+  return (
+    <div className="container">
+      <h1>client page</h1>
+    </div>
+  );
+}
+
+export default ClientPage;
---
+++ src/app/craftsmen/[id]/page.tsx
@@ -0,0 +1,9 @@
+function CraftsmanPage({ params }: { params: { id: string } }) {
+  return (
+    <div className="container">
+      <h1>craftsman page</h1>
+    </div>
+  );
+}
+
+export default CraftsmanPage;
