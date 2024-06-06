113b50f373f87c62983d009276a987c18ed7b158
waseem <waseemazmy21@gmail.com>
2024-06-06 10:54:19 +0300

    Add client dashboard

--- src/app/craftsman/page.tsx
+++ src/app/craftsman/page.tsx
@@ -1,9 +1,57 @@
-function CraftsmanPage({ params }: { params: { id: string } }) {
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { buttonVariants } from "@/components/ui/button";
+import JobCard from "@/components/client-dashboard/job-card";
+import Link from "next/link";
+import { translateServerMessage } from "@/utils/utils";
+
+const ClientDashboard = () => {
+  const [jobs, setJobs] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const response = await axios.get("http://localhost:7000/api/jobs", {
+          headers: {
+            "x-auth-token": token,
+          },
+        });
+
+        setJobs(response.data);
+      } catch (e: any) {
+        setError(e.response?.data?.message || "An error occurred");
+        alert(translateServerMessage(error));
+      }
+    };
+
+    fetchJobs();
+  }, [error]);
+
   return (
-    <div className="container">
-      <h1>craftsman page</h1>
+    <div className="container py-8">
+      <div className="flex justify-between gap-4">
+        <h1 className="typography-h3">مساحة العمل الخاصة بك</h1>
+        <Link
+          className={buttonVariants({ variant: "outline", class: "mb-4" })}
+          href="/craftsman/post-job"
+        >
+          نشر وظيفة
+        </Link>
+      </div>
+      <div className="max-w-lg">
+        {jobs.length > 0 ? (
+          jobs.map((job) => <JobCard key={job.id} job={job} />)
+        ) : (
+          <p>لم تنشر أي وظيفة بعد</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default CraftsmanPage;
+export default ClientDashboard;
---
+++ src/components/client-dashboard/job-card.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+const JobCard = ({ job }: { job: any }) => {
+  return (
+    <div className="border-b border-t border-gray-200 py-4">
+      <div className="mb-2 flex items-center justify-between">
+        <h2 className="text-xl font-bold">{job.title}</h2>
+        <p className="text-lg font-semibold text-green-500">{job.budget} ج.م</p>
+      </div>
+      <div className="text-sm text-gray-600">
+        <p>{job.description}</p>
+      </div>
+      <div className="mt-2 text-gray-800">
+        <p>
+          <strong>الموقع:</strong> {job.location}
+        </p>
+        <p>
+          <strong>المدة:</strong> {job.duration}
+        </p>
+        <p>
+          <strong>الفئة:</strong> {job.category}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
